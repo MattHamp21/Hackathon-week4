fc3e97e20649b60da49164f81f0832905e378da9
Matt Hamp <Matthewhamp3@gmail.com>
2022-06-11 15:13:31 -0600

    yup

--- client/src/Character/CharacterShow.js
+++ client/src/Character/CharacterShow.js
@@ -1,6 +1,7 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { Link, useNavigate, useParams } from "react-router-dom";
 import axios from "axios";
+import Characters from "./Character";
 
 export default function CharacterShow() {
   const params = useParams();
@@ -8,6 +9,15 @@ export default function CharacterShow() {
   const [characters, setCharacters] = useState(null);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
+
+  useEffect(() => {
+    getCharacters();
+  }, []);
+
+  const deleteCharacter = (id) => {
+    let newCharacter = characters.filter((c) => c.id !== id);
+    setCharacters(newCharacter);
+  };
 
   const getCharacters = async () => {
     try {
@@ -21,11 +31,28 @@ export default function CharacterShow() {
     }
   };
 
+  const renderUser = () => {
+    if (loading) {
+      return <p>loading</p>;
+    }
+    if (error) {
+      return <p>{JSON.stringify(error)}</p>;
+    }
+    return characters.map((c) => {
+      return (
+        <div key={c.id}>
+          <h1>Name: {c.name}</h1>
+          <button onClick={() => deleteCharacter(c.id)}>delete</button>
+        </div>
+      );
+    });
+  };
   return (
     <div>
       <h1>Show Character</h1>
       <p> showing Character with id {params.id}</p>
-      <p> Name {params.name}</p>
+      <p> Name: {params.name}</p>
+      <div>{renderUser()}</div>
       <Link to="/Character">Go back to character</Link>
     </div>
   );
