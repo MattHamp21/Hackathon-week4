27f75792b06f18b976309904c58a0e3c457f813d
Jon_Ogden <jogden1863@gmail.com>
2022-06-11 13:13:30 -0500

    games model functional

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,20 @@
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
-import PageWrapper from "./Pagewrapper";
+import PageWrapper from "./PageWrapper";
 import Home from "./Home";
+import Game from "./Game/Game";
+import Character from "./Character/Character";
+import Weapon from "./Weapon/Weapon";
 
 function App() {
   return (
     <Routes>
       <Route element={<PageWrapper />}>
         <Route index element={<Home />} />
-        {/* <Route path="/Game" element={<Game />} />
+        <Route path="/games" element={<Game />} />
         <Route path="/Character" element={<Character />} />
         <Route path="/Weapon" element={<Weapon />} />
-        <Route path="/Bonus" element={<Bonus />} /> */}
+        {/* <Route path="/Bonus" element={<Bonus />} />  */}
       </Route>
     </Routes>
   );
